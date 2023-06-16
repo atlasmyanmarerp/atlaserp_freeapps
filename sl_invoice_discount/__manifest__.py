88cec9732ab48d63087a09fc9624e135a65aec4f
System Administrator <root@Users-MacBook-Pro.local>
2023-06-16 16:30:41 +0630

    Fix Error

--- sl_invoice_discount/__manifest__.py
+++ sl_invoice_discount/__manifest__.py
@@ -8,24 +8,15 @@
 
     'author': "Atlas Myanmar ERP",
     'website': "https://www.atlasmyanmarerp.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'version': '16.0',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'account'],
-    'live_test_url': 'https://www.youtube.com/watch?v=QCtEe-zsCtQ'
-
-    # always loaded
+    'live_test_url': 'https://www.youtube.com/watch?v=QCtEe-zsCtQ',
     'data': [
         # 'security/ir.model.access.csv',
         'data/data.xml',
         'views/account_move_inherit.xml',
         # 'views/purchase_order_inherit_view.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
